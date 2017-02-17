4020b93dc1211475e22e5ab32b4e9cbe0279f0e0
Steven Ensslen <steven.ensslen@gmail.com>
2017-02-17 14:42:02 +1300

    corrects defects in package framework

--- app/spy_deploy.sql
+++ app/spy_deploy.sql
@@ -6,10 +6,6 @@ CREATE OR REPLACE PACKAGE spy_deploy AS
     
 	PROCEDURE set_up (procedure_name ORA_NAME, object_type ORA_NAME);
 	PROCEDURE tear_down (procedure_name ORA_NAME);
-    
-    FUNCTION generate_rename_ddl(old_name ORA_NAME, new_name ORA_NAME) RETURN VARCHAR2;
-    
-    FUNCTION generate_spy(spy spy_procedures%rowtype) RETURN VARCHAR2;
-    
+        
 END spy_deploy;
 
